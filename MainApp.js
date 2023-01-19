13fe2fcfd299fbfdbdbd960fe64912d8a21fd475
tuantrminh1997 <122778674+TranMinhTuan-Coding@users.noreply.github.com>
2023-01-20 00:04:05 +0700

    commit ! 00:03' !

---
+++ MainApp.js
@@ -0,0 +1,23 @@
+$('.brendan-eich-title button').click(function () {
+    $('.brendan-eich-title').toggleClass('title-click');
+    
+    $('.brendan-eich-title-h3').toggleClass('h3-click');
+
+    $('.brendan-eich-content').slideToggle();
+});
+
+$('.john-resign-title button').click(function () {
+    $('.john-resign-title').toggleClass('title-click');
+    
+    $('.john-resign-title-h3').toggleClass('h3-click');
+    
+    $('.john-resign-content').slideToggle();
+});
+
+$('.mark-zuckerberg-title button').click(function () {
+    $('.mark-zuckerberg-title').toggleClass('title-click');
+
+    $('.mark-zuckerberg-title-h3').toggleClass('h3-click');
+
+    $('.mark-zuckerberg-content').slideToggle();
+});
